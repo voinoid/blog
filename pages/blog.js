8e276d2a4fa24c972009e803047136c9c04aaf41
Jonathan Nye <nyejon@gmail.com>
2022-07-04 22:45:04 +0200

    Sort out headings

--- pages/blog.js
+++ pages/blog.js
@@ -24,7 +24,7 @@ export default function Blog({ posts, initialDisplayPosts, pagination }) {
         posts={posts}
         initialDisplayPosts={initialDisplayPosts}
         pagination={pagination}
-        title="all posts"
+        title="All posts"
       />
     </>
   )
