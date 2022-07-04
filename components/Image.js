70194f102c0ba76ef8a5d91e2a6741c33ad5f722
Jonathan Nye <nyejon@gmail.com>
2022-07-04 22:21:05 +0200

    Update image loader to work with the obsidian publish paths

--- components/Image.js
+++ components/Image.js
@@ -1,7 +1,11 @@
 import Image from 'next/image'
 
 const normalizeSrc = (src) => {
-  return src[0] === '/' ? src.slice(1) : src
+  // if ../../public in src, remove it
+  if (src.includes('../../public')) {
+    return src.replace('../../public', '')
+  }
+  return src
 }
 
 const cloudflareImageLoader = ({ src, width, quality }) => {
@@ -13,9 +17,12 @@ const cloudflareImageLoader = ({ src, width, quality }) => {
 }
 
 export default function Img(props) {
+  // This normalizes the src to be relative to the public folder
+  var src = normalizeSrc(props.src)
+
   if (process.env.NODE_ENV === 'development') {
-    return <Image unoptimized={true} {...props} />
+    return <Image unoptimized={true} {...props} src={src} />
   } else {
-    return <Image {...props} loader={cloudflareImageLoader} />
+    return <Image {...props} src={src} loader={cloudflareImageLoader} />
   }
 }
